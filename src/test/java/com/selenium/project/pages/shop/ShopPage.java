788c8e752a1ed174680df9b2d2339b8f7c4c7c77
gioinn <gioinn@viettel.com.vn>
2022-08-30 15:57:19 +0700

    cm03008

--- src/test/java/com/selenium/project/pages/shop/ShopPage.java
+++ src/test/java/com/selenium/project/pages/shop/ShopPage.java
@@ -3,14 +3,7 @@ package com.selenium.project.pages.shop;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
 
 public class ShopPage {
   private WebDriver driver;
--- src/test/java/com/selenium/project/testcases/myaccount/BaseTest.java
+++ src/test/java/com/selenium/project/testcases/myaccount/BaseTest.java
@@ -1,19 +1,20 @@
 package com.selenium.project.testcases.myaccount;
 
-import com.selenium.project.common.Helper;
 import com.selenium.project.common.SetupDriver;
 import com.selenium.project.pages.MenuBar;
 import com.selenium.project.pages.myaccount.LoginPage;
+import com.selenium.project.pages.myaccount.RegisterPage;
 import org.testng.annotations.BeforeMethod;
 
 public class BaseTest extends SetupDriver {
   private MenuBar menuBar;
   protected LoginPage loginPage;
-  private Helper helper;
+  protected RegisterPage registerPage;
 
   @BeforeMethod
   public void beforeBaseTest() throws Exception {
     menuBar = new MenuBar(driver);
+    registerPage = new RegisterPage(driver);
     //Thread.sleep(3000);
     loginPage = menuBar.OpenMyAccountPage();
   }
