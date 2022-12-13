8b46410def05991148836cf0d43f14b7be620f51
nngocgioi2712 <nguyenngocgioi127@gmail.com>
2022-12-13 22:43:05 +0700

    update 1312

--- src/test/java/com/selenium/project/pages/Advertisement.java
+++ src/test/java/com/selenium/project/pages/Advertisement.java
@@ -1,24 +1,22 @@
 package com.selenium.project.pages;
 
 import com.selenium.project.utils.Log;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class Advertisement {
     private static  String urlAd = "https://practice.automationtesting.in/#google_vignette";
-    @FindBy(css = "#dismiss-button")
-    private static WebElement btn_closeAd;
+    private static By oj_btn_closed = By.id("dismiss-button");
 
-    @FindBy(css = ".dismiss-button")
-    private  static WebElement btn_close;
     public static void closeAd(WebDriver driver){
+
         if(driver.getCurrentUrl().equals(urlAd)){
             driver.switchTo().frame("aswift_7");
-            if(btn_closeAd != null){
-                btn_closeAd.click();
-            } else if(btn_close != null) {
-                btn_close.click();
+            if(driver.findElement(oj_btn_closed) != null){
+                driver.findElement(oj_btn_closed).click();
+            } else {
             }
             Log.info("Ad was closed");
             driver.switchTo().parentFrame();
